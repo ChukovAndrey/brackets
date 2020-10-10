253ed3372da5c122b26c0a4ccbb3fdc4049af429
ChukovAndrey <chukov.info@gmail.com>
2020-10-10 12:30:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let transformedString = str;
+  for (let i = 0; i <= str.length / 2; i += 1) {
+    for (let item of bracketsConfig) {
+      transformedString = transformedString.replace(`${item[0]}${item[1]}`, '');
+    }
+  }
+  return transformedString.length === 0;
 }
